def19e3f19cd9a0b825195ff7258cfe008a48953
Sofus Albertsen <soa@praqma.com>
2019-12-02 13:21:40 +0100

    Added CI part for this microservice
    Three jobs; unit test, Docker image build and component testing.
    Added Docker image and Docker compose for testing.
    Updated the CI scripts to take in env. variables for pushing to Dockerhub

---
+++ test/test_age_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import unittest
+import os
+import requests
+import re
+
+class TestAge(unittest.TestCase):
+    url = os.getenv('SERVICE_URL', 'http://127.0.0.1:8080')
+
+    def test_response_format(self):
+        response = requests.get(self.url, timeout=5)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.encoding, 'utf-8')
+        self.assertTrue(re.match('^\d+$', response.text))
+
+    def test_range(self):
+        response = requests.get(self.url, timeout=1)
+        age = int(response.text)
+        self.assertTrue(age>0)
+        self.assertTrue(age<100)
+
+if __name__ == '__main__':
+    unittest.main()

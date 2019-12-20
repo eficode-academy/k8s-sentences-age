c34f7c3895abfb23f267e9033f35c00b15026b04
Jonas Vinther <jrvinther@gmail.com>
2019-12-20 22:00:01 +0100

    Added prometheus metrics

--- app/age.go
+++ app/age.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
     "math/rand"
     "time"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+    httpReqs = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "age_http_requests_total",
+			Help: "How many HTTP requests processed",
+		},
+		[]string{"code", "method"},
+	)
 )
 
 func GetAge(min int, max int)(int) {
@@ -13,15 +25,23 @@ func GetAge(min int, max int)(int) {
 	return rand.Intn(max - min) + min
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    min := 0
-    max := 100
-    age := GetAge(min, max)
+func handler(httpReqs *prometheus.CounterVec) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        min := 0
+        max := 100
+        age := GetAge(min, max)
 
-	fmt.Fprintf(w, "%d", age)
+        fmt.Fprintf(w, "%d", age)
+
+        m := httpReqs.WithLabelValues("200", "GET")
+        m.Inc()
+    }
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+    prometheus.MustRegister(httpReqs)
+
+    http.HandleFunc("/", handler(httpReqs))
+    http.Handle("/metrics", promhttp.Handler())
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+docker run --rm -it -v $(pwd)/app:/go golang:1.13.4 go get -d -v
 docker run --rm -it -v $(pwd)/app:/go golang:1.13.4 go test . -v

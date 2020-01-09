9f8d075c65edc6216db98a163362091750d4996c
Sofus Albertsen <soa@praqma.com>
2020-01-09 11:05:45 +0100

    Adding local build capabilities

--- test/component-test.sh
+++ test/component-test.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-docker_username=$docker_username docker-compose -p ci up --exit-code-from sut --build
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+docker_username=$DockerRepo docker-compose -p ci up --build --exit-code-from sut
